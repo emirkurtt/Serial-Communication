cba4a5164b4f8f1e7827bcd716f5da54212b737d
emirkurtt <99839084+emirkurtt@users.noreply.github.com>
2022-12-24 21:05:12 +0300

    Update spi_master.vhd
    Memory interface will be utilized, initial steps are done, the remaining will be completed later. 
    Initial steps: mode_determination etc
    remaining steps: slave enable and continuity of memory structure. 
    a memory will also be added.

--- Data/SPI/spi_master.vhd
+++ Data/SPI/spi_master.vhd
@@ -8,24 +8,32 @@
 --  Mode 3   => mode input = "11" =>    RISING_EDGE             ||  FALLING_EDGE                     -- NEGATIVE CLOCK POLARITY   
 --
 --
+-- There will be two memories
+-- One for config and write data
+-- Other for only saving data coming from MISO
+-- For the first mem:
+-- D_in(9 downto 0):
+-- D_in(9 downto 8) = "00" : send_data
+--                  = "01" : enabled slave and CS active high/low
+--                  = "10" : reserved
+--                  = "11" : mode determination
 library IEEE;
 use IEEE.std_logic_1164.ALL;
 use IEEE.numeric_std.ALL;
 
 entity SPI_master is 
     generic(
-        DATA_WIDTH      : integer := 8;
-        clk_per_bit     : integer := 4
+        DATA_WIDTH          : integer := 8;
+        INSTRUCTION_WIDTH   : integer := 2;     -- instruction width is 2 for now
+        SLAVE_COUNT         : integer := 1;
+        clk_per_bit         : integer := 4
     );
     port(
         clk     : in    std_logic;
         rst_n   : in    std_logic;
-        mode    : in    std_logic_vector(1 downto 0);
-        D_in    : in    std_logic_vector(DATA_WIDTH-1 downto 0);
-        DV      : in    std_logic;                                  -- DATA VALID INPUT
+        D_in    : in    std_logic_vector(DATA_WIDTH + INSTRUCTION_WIDTH -1 downto 0);
+        spi_en  : in    std_logic;                                  -- DATA VALID INPUT
         MISO    : in    std_logic;
-        i_CS1   : in    std_logic;
-        i_CS2   : in    std_logic;
         MOSI    : out   std_logic;
         SCLK    : out   std_logic;
         Tx_busy : out   std_logic;
@@ -33,62 +41,117 @@ entity SPI_master is
         Rx_busy : out   std_logic;
         Rx_end  : out   std_logic;
         Rx_byte : out   std_logic_vector(DATA_WIDTH-1 downto 0);
-        CS1     : out   std_logic;
-        CS2     : out   std_logic
+        CS      : out   std_logic_vector(SLAVE_COUNT-1 downto 0);
+        errors  : out   std_logic_vector(3 downto 0)                -- errors(0) <= '0' if mode is determined correctly, else '1'
+                                                                    -- errors(1) <= '0' if enabled slave is determined correctly including active state(high/low), else '1'
+                                                                    -- errors(2) <= '0' if mode is determined correctly, else '1'
+                                                                    -- errors(3) <= '0' if mode is determined correctly, else '1'
     );
 end entity SPI_master;
 
 architecture arch of SPI_master is
+
+    constant MODE_0             : std_logic_vector(7 downto 0)  :=  "00000000";
+    constant MODE_1             : std_logic_vector(7 downto 0)  :=  "00000001";
+    constant MODE_2             : std_logic_vector(7 downto 0)  :=  "00000010";
+    constant MODE_3             : std_logic_vector(7 downto 0)  :=  "00000011";
+
+    type SCLK_FSM is (ST_IDLE,ST_DETERMINE_MODE);
     type TX_FSM is (ST_TX_IDLE,ST_TX_SEND,ST_TX_DONE);
     type RX_FSM is (ST_RX_IDLE,ST_RX_RECV,ST_RX_DONE);
     signal TX_State             : TX_FSM;
     signal RX_State             : RX_FSM;
     signal SCLK_buf             : std_logic;
-    signal clk_flag             : std_logic_vector (1 downto 0);
+    signal mode_ok              : std_logic;                                            -- shows if mode determined, transactions in RX and TX are started with this 
+    signal mode                 : std_logic_vector  (1 downto 0);
+    signal clk_flag             : std_logic_vector  (1 downto 0);
     signal MISO_Data_Buffer     : std_logic_vector (DATA_WIDTH-1 downto 0);
     signal clk_counter          : unsigned (10 downto 0);
     signal MOSI_Index_Ctr       : unsigned (4 downto 0);
     signal MISO_Index_Ctr       : unsigned (4 downto 0);
-    
 begin
     SCLK    <= SCLK_buf;
     -- clk generator
     sclk_gen : process(rst_n,clk) is
     begin
         if rst_n = '0' then
-            if mode(1) = '0' then           -- POSITIVE CLOCK POLARITY CASE
-                SCLK_buf    <= '0';
-            else                            -- NEGATIVE CLOCK POLARITY CASE
-                SCLK_buf    <= '1';
-            end if;
-            clk_flag    <= "00";
-            clk_counter     <= (others => '0');            
+            mode_ok         <= '0';
+            mode            <= "00";
+            clk_flag        <= "00";
+            clk_counter     <= (others => '0');
+            state_sclk      <= ST_IDLE;               
         elsif RISING_EDGE(clk) then
-            if to_integer(clk_counter) = clk_per_bit then
-                if SCLK_buf = '0' then          -- RISING EDGE OF SCLK
-                    clk_flag        <= "01";                                     
-                else                            -- FALLING EDGE OF SCLK
-                    clk_flag        <= "10";                                     
-                end if;
-                clk_counter     <= (others => '0');
-                SCLK_buf        <= not SCLK_buf;
+            if spi_en = '1' then
+                case state_sclk is 
+                    when ST_IDLE            =>
+                        SCLK_buf    <= 'Z';
+                        if D_in(9 downto 8) = "11" then
+                            case D_in(7 downto 0) is
+                                when MODE_0 =>    
+                                    mode        <= "00";
+                                    SCLK_buf    <= '0';
+                                when MODE_1 =>   
+                                    mode        <= "01";
+                                    SCLK_buf    <= '0';
+                                when MODE_2 =>                            
+                                    mode        <= "10";
+                                    SCLK_buf    <= '1';
+                                when MODE_3 =>
+                                    mode        <= "11";
+                                    SCLK_buf    <= '1';
+                                when others =>
+                                    errors(0) <= '1';
+                            end case;
+                            state_sclk  <= ST_START_SCLK_GEN;
+                            mode_ok <= '1';
+                        else
+                            mode_ok <= '0';
+                        end if;
+                    when ST_START_SCLK_GEN  =>
+                        if to_integer(clk_counter) = clk_per_bit then
+                            if SCLK_buf = '0' then          -- RISING EDGE OF SCLK
+                                clk_flag        <= "01";                                     
+                            else                            -- FALLING EDGE OF SCLK
+                                clk_flag        <= "10";                                     
+                            end if;
+                            clk_counter     <= (others => '0');
+                            SCLK_buf        <= not SCLK_buf;
+                        else
+                            clk_flag        <= "00";
+                            clk_counter     <= clk_counter + 1;
+                        end if;
+                end case;
             else
+                state_sclk      <= ST_IDLE;
+                mode_ok         <= '0';
+                mode            <= "00";
                 clk_flag        <= "00";
-                clk_counter     <= clk_counter + 1;
+                clk_counter     <= (others => '0');
+            end if;
+        end if;   
+    end process;
+    
+    
+    -- CONFIGURATION PROCESS, when completed, a conf_done signal will be sent to RX and TX processes
+    CONF_proc   : process(rst_n,clk) is
+    begin
+        if rst_n = '0' then
+           CS   <= (others => 'Z');
+        elsif RISING_EDGE(clk) then 
+            if spi_en = '1' then
+                conf_state       <= ST_WAIT_MEM;
             end if;
         end if;
     end process;
-    
     -- XMISSION PROCESS
     TX_proc: process(rst_n,clk) is 
     begin
         if rst_n = '0' then
             MOSI                <= 'Z';
             MOSI_Index_Ctr      <= (others => '0');
+            CS                  <= (others => 'Z');
             Tx_busy             <= '0';
             Tx_end              <= '0';
-            CS1                 <= '1'; -- Active low
-            CS2                 <= '1';
         elsif RISING_EDGE(clk) then
             case TX_State is 
                 when ST_TX_IDLE =>
@@ -96,14 +159,23 @@ begin
                     MOSI_Index_Ctr      <= (others => '0');
                     Tx_end              <= '0';
                     Tx_busy             <= '0';
-                    if DV = '1' then
-                        CS1         <= i_CS1;
-                        CS2         <= i_CS2;
-                        TX_State    <= ST_TX_SEND;
-                        Tx_busy     <= '1';
+                    CS                  <= (others => 'Z');
+                    if spi_en = '1' then
+                        mem_addr_buf    <= std_logic_vector(unsigned(mem_addr_buf) + 1);
+                        TX_State        <= ST_TX_WAIT_MEM;
+                        Tx_busy         <= '1';
+                    end if;
+                when ST_TX_WAIT_MEM =>      -- wait mem
+                     TX_State        <= ST_TX_WAIT_CONF_SLAVE_ENABLE;
+                when ST_TX_WAIT_CONF_SLAVE_ENABLE =>                                       -- mem(0) consists of the slave to be enabled
+                    if D_in(9 downto 8) = "01" then
+                       if D_in(7) = '0' then                                                        -- CS is active low
+                            CS  <= others(to_integer(unsigned(D_in(6 downto 0))) => '0' , others => '1'); 
+                       else                                                                         -- CS is active high 
+                            CS  <= others(to_integer(unsigned(D_in(6 downto 0))) => '1' , others => '0'); 
+                       end if;
                     else
-                        CS1 <= '1';
-                        CS2 <= '1';
+                        errors(1)   <= '1';
                     end if;
                 when ST_TX_SEND =>
                     if clk_flag = "01" and (mode = "00" or mode = "11" )then            -- sampling is done in RISING_EDGE of the clock for modes 0 and 3
@@ -148,7 +220,7 @@ begin
                     MISO_Index_Ctr      <= (others => '0');
                     MISO_Data_Buffer    <= (others => '0');
                     Rx_end              <= '0';
-                    if DV = '1' then
+                    if spi_en = '1' then
                         RX_State            <= ST_RX_RECV;
                         Rx_busy             <= '1';
                     else
